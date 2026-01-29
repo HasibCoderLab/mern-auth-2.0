26b4210a77a95cfa5b32cc89695bee349c99b928
Hasib Hasan <coderhasibh@gmail.com>
2026-01-29 15:03:43 +0600

    create user

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,22 +1,29 @@
 import { User } from "../models/userModel.js";
+import bcrypt from "bcryptjs";
 
 
-export const register = async (req,res) =>{
-try {
-const {name,password,email} = req.body
-    if (!name|| !password ||! email) {
-        return res.status(400).json({message:"Send all details"});
-    };
-    // ======= Check User ====
-    const existUser = User.findOne({email});
-    if (existUser) {
-        return res.status(400).json({message:"user already exist"});
+export const register = async (req, res) => {
+    try {
+        const { name, password, email } = req.body
+        if (!name || !password || !email) {
+            return res.status(400).json({ message: "Send all details" });
+        };
+        // ======= Check User ====
+        const existUser = User.findOne({ email });
+        if (existUser) {
+            return res.status(400).json({ message: "user already exist" });
+        }
+
+        // =============  Password Hass =====
+        const hassPassword = await bcrypt.hash(password, 11);
+
+        // =============  Password Hass =====
+        const user = User.create({
+            name, password: hassPassword, email
+        });
+
+
+    } catch (error) {
+        return res.status(500).json({ message: "Internal Server Error" });
     }
-
-    // =============  Password Hass =====
-    const hassPassword = await bc
-
-} catch (error) {
-    return res.status(500).json({message:"Internal Server Error"});
-}
 }
