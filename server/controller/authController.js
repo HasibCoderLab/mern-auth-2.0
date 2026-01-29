bee08386923a25326d55cd75769dab0bb3cf44de
Hasib Hasan <coderhasibh@gmail.com>
2026-01-29 18:43:51 +0600

    login api building

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,3 +1,4 @@
+import { generateToken } from "../config/token.js";
 import { User } from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 
@@ -9,7 +10,7 @@ export const register = async (req, res) => {
             return res.status(400).json({ message: "Send all details" });
         };
         // ======= Check User ====
-        let  existUser = await User.findOne({ email });
+        let existUser = await User.findOne({ email });
         if (existUser) {
             return res.status(400).json({ message: "user already exist" });
         }
@@ -17,19 +18,63 @@ export const register = async (req, res) => {
         // =============  Password Hass =====
         const hassPassword = await bcrypt.hash(password, 11);
 
-        // =============  Password Hass =====
+
         const user = User.create({
-            name, password: hassPassword, email
+            name,
+            email,
+            password: hassPassword
         });
+
+
+        console.log(user);
+
+
+
+        // ==========  Token + cookie  ===========
+        let token = await generateToken(user._id);
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+
+
+
         return res.status(201).json({
-            user:{
-name ,email
+            user: {
+                name, email
             }
+
         }
-           
-        )
+
+        );
 
     } catch (error) {
-        return res.status(500).json({ message: "Internal Server Error" });
+        return res.status(500).json({ message: "Internal Server Error", });
+    }
+}
+
+
+// ==============  Login ========
+export const login = async () => {
+    const { email, password } = req.body
+    if (!email || !password) {
+        return res.json({ success: false, message: 'Email and Password are required ' })
+    }
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid Emeil" });
+
+        }
+        // ============ Password Compare ==============
+        let isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Incorrect Password" });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: "Internal Server Error", });
     }
 }
