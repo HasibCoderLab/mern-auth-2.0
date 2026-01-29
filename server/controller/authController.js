80afd2cd9ea377a8a2eaea806122ef4b78859440
Hasib Hasan <coderhasibh@gmail.com>
2026-01-29 14:41:48 +0600

    check user

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,11 +1,17 @@
+import { User } from "../models/userModel.js";
 
 export const register = async (req,res) =>{
 try {
 const {name,password,email} = req.body
     if (!name|| !password ||! email) {
         return res.status(400).json({message:"Send all details"})
+    };
+    // ======= Check User ====
+    const existUser = User.findOne({email});
+    if (existUser) {
+        return res.status(400).json({message:"user already exist"})
     }
 } catch (error) {
-    return res.status(500).json({message:"Internal Server Error"})
+    return res.status(500).json({message:"Internal Server Error"});
 }
 }
