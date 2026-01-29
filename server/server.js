3ca4d373855ea7f27bbcf3a62c0bc289f4ed8f34
Hasib Hasan <coderhasibh@gmail.com>
2026-01-29 12:31:00 +0600

    creating model

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
 import express from "express";
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+import { connectDB } from "./config/database.js";
+
+dotenv.config()
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/",(req,res) =>{
+    res.send("API Done")
+})
+
+
+const port = process.env.PORT || 4000
+
+app.listen = (port , () =>{
+    connectDB()
+console.log(`Server Stated at ${port}`);
+
+});
