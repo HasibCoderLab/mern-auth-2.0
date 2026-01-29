52bc9f537fadbc7bec27fd9e1040058e35baae2b
Hasib Hasan <coderhasibh@gmail.com>
2026-01-29 14:54:12 +0600

    create token

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -4,13 +4,14 @@ export const register = async (req,res) =>{
 try {
 const {name,password,email} = req.body
     if (!name|| !password ||! email) {
-        return res.status(400).json({message:"Send all details"})
+        return res.status(400).json({message:"Send all details"});
     };
     // ======= Check User ====
     const existUser = User.findOne({email});
     if (existUser) {
-        return res.status(400).json({message:"user already exist"})
+        return res.status(400).json({message:"user already exist"});
     }
+    
 } catch (error) {
     return res.status(500).json({message:"Internal Server Error"});
 }
