79ddabe5ee6b2035509923ccd5dd56ee94acc6c9
Hasib Hasan <coderhasibh@gmail.com>
2026-02-03 15:27:46 +0600

    crate controller

---
+++ server/controller/useruserController.js
@@ -0,0 +1,21 @@
+import { User } from "../models/userModel";
+
+export const getUserData = async (req, res) => {
+    try {
+        const { userId } = req.body;
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(400).json({ success: false, message: "User not found" });
+        }
+
+        res.json({
+            success: true,
+            userData: {
+                name: user.name,
+                isAccountVerified: user.isAccountVerified
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({ success: false, message: error.message })
+    }
+}
