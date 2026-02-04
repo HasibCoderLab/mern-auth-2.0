3a6a723727a4572fa39ed21639e8f2a1286d95f1
Hasib Hasan <coderhasibh@gmail.com>
2026-02-04 11:17:36 +0600

    crate routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import {Routers,Route} from "react-router"
+import {Routes,Route} from "react-router"
+import Home from './pages/Home'
+import Login from './pages/Login'
+import EmailVerify from './pages/EmailVerify'
+import ResetPassword from './pages/ResetPassword'
 const App = () => {
   return (
     <div>
-      <Routers> 
-        <Route />
-      </Routers>
+      <Routes> 
+        <Route path='/' element={<Home/>}  />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/email-verify' element={<EmailVerify />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+       
+
+      </Routes>
     </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      
+      <h1 className='text-red-600'> Hello</h1>
     </div>
   )
 }
