e07c0004cb4a73176de1ab39e220a2d2815921f7
Hasib Hasan <coderhasibh@gmail.com>
2026-02-05 15:39:05 +0600

    solve import erroe

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import {assets} from "../assets/assets"
 const Navbar = () => {
   return (
     <div>
-      
+      <img src={assets.logo} alt="Logo" className='w-22 sm:w-32' />
     </div>
   )
 }
