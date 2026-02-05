b9f2078dbbadf30eb5489c574fdbc04d764cbe0c
Hasib Hasan <coderhasibh@gmail.com>
2026-02-05 16:16:17 +0600

    created header section

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Header = () => {
+  return (
+    <div className='flex flex-col items-center mt-20 px-4 text-center text-gray-800'>
+      <img src={assets.header_img} alt="Header-Img" className='w-36 h-36 rounded-full mb-6'/>
+
+      <h1 className='flex  items-center  gap-2 text-xl sm:text-3xl font-medium mb-2'> Hey !  Developer <img src={assets.hand_wave}  className="w-8 aspect-square" alt="hand_wave" /></h1>
+
+      <h2 className='text-3xl sm:text-5xl font-semibold mb-4'>Welcome to our app </h2>
+      <p className="mb-8 max-w-md">Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis maiores ab repudiandae est modi? Dignissimos exercitationem ut necessitatibus enim pariatur.</p>
+
+      <button className="border border-gray-500  rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all cursor-pointer">Get Started</button>
+    </div>
+  )
+}
+
+export default Header
