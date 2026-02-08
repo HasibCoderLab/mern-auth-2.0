2f470666a3735d2c465818ecfeb1204b4b3b659c
Hasib Hasan <coderhasibh@gmail.com>
2026-02-08 22:02:21 +0600

    solve Spelling issue

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { assets } from '../assets/assets';
 
 const Login = () => {
- const [state,setState] = useState('sign up');
+ const [state,setState] = useState('Sign Up');
   return (
     <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-linear-to-br from-blue-200 to-purple-400'>
       <img 
