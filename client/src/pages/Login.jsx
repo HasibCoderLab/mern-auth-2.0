0bef0dd68667d08393ae79bdc75248fc049ee14a
Hasib Hasan <coderhasibh@gmail.com>
2026-02-09 22:02:04 +0600

    img optimization

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
   return (
     <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-linear-to-br from-blue-200 to-purple-400'>
       <img
-        className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'
+        className='absolute left-5 sm:left-20 top-5 w-12 sm:w-32 cursor-pointer'
         src={assets.logo1} alt="" />
       <div className=" bg-slate-900 p-10 rounded-lg text-indigo-300 w-full sm:w-96 text-sm">
         <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</h2>
