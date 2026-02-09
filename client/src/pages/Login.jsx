ca5e4e7687bbc8e5dbcc1a2fdb653ee749fae985
Hasib Hasan <coderhasibh@gmail.com>
2026-02-09 22:14:29 +0600

    triger

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
   return (
     <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-linear-to-br from-blue-200 to-purple-400'>
       <img
-        className='absolute left-5 sm:left-20 top-5 w-12 sm:w-32 cursor-pointer'
+        className='absolute left-5 sm:left-20 top-5  w-12 sm:w-32 cursor-pointer'
         src={assets.logo1} alt="" />
       <div className=" bg-slate-900 p-10 rounded-lg text-indigo-300 w-full sm:w-96 text-sm">
         <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</h2>
@@ -38,7 +38,10 @@ const Login = () => {
               type="password" placeholder='Password' required />
           </div>
           <p className='mb-4 text-indigo-500 cursor-pointer'>Forget Password ?</p>
-          <button className='w-full py-2.5 rounded-full bg-linear-to-r from-indigo-500 to-indigo-900 text-white font-medium'>{state}</button>
+          <button className='w-full py-2.5 rounded-full bg-linear-to-r from-indigo-500 to-indigo-900 text-white font-medium cursor-pointer'>{state}</button>
+          <p className="text-gray-400 text-center text-xs mt-4">Alread have an accunt? {``}
+            <span className="text-blue-400 cursor-pointer">Login here</span> </p>
+          
         </form>
       </div>
     </div>
