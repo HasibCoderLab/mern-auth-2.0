7f545f634145af8fba1dec7c10ef6f186e8b3f21
Hasib Hasan <coderhasibh@gmail.com>
2026-02-11 11:55:21 +0600

    using states , conditionm

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router';
 import { assets } from '../assets/assets';
 
 const Login = () => {
-   const navigate = useNavigate();
+  const navigate = useNavigate();
   const [state, setState] = useState('Sign Up');
   return (
     <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-linear-to-br from-blue-200 to-purple-400'>
@@ -17,13 +17,19 @@ const Login = () => {
         {/*  Form Section */}
 
         <form action="">
+
           {/* Name */}
-          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
-            <img src={assets.person_icon} alt="" />
-            <input
-              className='bg-transparent outline-none'
-              type="text" placeholder='Full Name' required />
-          </div>
+
+          {state === 'Sign Up' && (
+            <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+              <img src={assets.person_icon} alt="" />
+              <input
+                className='bg-transparent outline-none'
+                type="text" placeholder='Full Name' required />
+            </div>
+          )}
+
+
           {/* Email */}
           <div
             className='mb-4 flex items-center gap-3 w-full px-5 py-2.5
@@ -41,9 +47,21 @@ const Login = () => {
           </div>
           <p className='mb-4 text-indigo-500 cursor-pointer'>Forget Password ?</p>
           <button className='w-full py-2.5 rounded-full bg-linear-to-r from-indigo-500 to-indigo-900 text-white font-medium cursor-pointer'>{state}</button>
-          <p className="text-gray-400 text-center text-xs mt-4">Already have an accunt? {``}
-            <span className="text-blue-400 cursor-pointer">Login here</span> </p>
-          
+
+          {/*  Condition For state */}
+
+          {
+            state === 'Sign Up' ?
+              (<p className="text-gray-400 text-center text-xs mt-4">Already have an accunt? {``}
+                <span onClick={()=> setState("Login")} className="text-blue-400 cursor-pointer">Login here</span> </p>)
+              :
+              (<p className="text-gray-400 text-center text-xs mt-4">Don't have an accunt? {``}
+                <span onClick={()=> setState('Sign Up')} className="text-blue-400 cursor-pointer underline">Sign up</span> </p>)
+          }
+
+
+
+
         </form>
       </div>
     </div>
