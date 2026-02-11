94484464f48c88c47d0bdb987971ca77430ead09
Hasib Hasan <coderhasibh@gmail.com>
2026-02-11 14:44:54 +0600

    navifate with reacr-router

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -12,10 +12,15 @@ const Login = () => {
   return ( 
     <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-linear-to-br from-blue-200 to-purple-400'> 
       <img 
+      onClick={()=>navigate('/')}
         className='absolute left-5 sm:left-20 top-0  w-12 sm:w-32 cursor-pointer' 
-        src={assets.logo1} alt="" /> 
+        src={assets.logo1} alt="" 
+      /> 
+
       <div className=" bg-slate-900 p-10 rounded-lg text-indigo-300 w-full sm:w-96 text-sm"> 
-        <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</h2> 
+        <h2 className='text-3xl font-semibold text-white text-center mb-3'>{
+        state === 'Sign Up' ? 'Create Account' : 'Login'
+        }</h2> 
  
         <p className='text-center text-sm mb-6'>{state === 'Sign Up' ? 'Create your account' : 'Login to your account'}</p> 
         {/*  Form Section */} 
@@ -57,7 +62,9 @@ const Login = () => {
               type="password" placeholder='Password' required /> 
           </div> 
 
-          <p className='mb-4 text-indigo-500 cursor-pointer'>Forget Password ?</p> 
+          <p onClick={()=>navigate('/reset-password')}
+          className='mb-4 text-indigo-500 cursor-pointer'>Forget Password ?
+          </p> 
           <button className='w-full py-2.5 rounded-full bg-linear-to-r from-indigo-500 to-indigo-900 text-white font-medium cursor-pointer'>{state}</button> 
  
           {/*  Condition For state */} 
