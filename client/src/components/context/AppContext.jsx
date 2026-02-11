dbcea0f7bddb7497863bf099c1139b5edd6d09dd
Hasib Hasan <coderhasibh@gmail.com>
2026-02-11 16:06:01 +0600

    raping data with context

--- client/src/components/context/AppContext.jsx
+++ client/src/components/context/AppContext.jsx
@@ -1,15 +1,27 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
 
-const value = {
-    
-}
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const [isLoggedin, setIsLoggedin] = useState(false);
+    const [userData, setUserData] = useState(false);
+
+
+
+    const value = {
+        backendUrl,
+        isLoggedin,
+        setIsLoggedin,
+        userData,
+        setUserData
+
+    }
+
     return (
         <AppContext.Provider value={value}>
-   {props.children}
+            {props.children}
         </AppContext.Provider>
     )
 }
